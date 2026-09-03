4815548f697b3284612055e378ba35a1512b46e6
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-03 08:27:09 -0700

    style: load image presentation layer

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import AppRoutes from './routes'
 import './index.css'
 import './multipage.css'
+import './image-polish.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
