5988adef193787f7a42f8e5edb6fd2e2c8668604
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-03 21:58:31 -0700

    style: load Eternal Praise product polish

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import './brand-themes.css'
 import './contact-polish.css'
 import './water-premium.css'
 import './eternal-praise.css'
+import './eternal-praise-polish.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
